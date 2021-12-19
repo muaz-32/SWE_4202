668d3775d6ab97c38461e70cccf7334e872fc51a
muaz-32 <96006730+muaz-32@users.noreply.github.com>
2021-12-19 09:42:46 +0600

    Add files via upload

---
+++ Student-Teacher System/Student-Teacher System/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace Student_Teacher_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button_stu_save = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox_stu_ID = new System.Windows.Forms.TextBox();
+            this.textbox_tchr_salary = new System.Windows.Forms.TextBox();
+            this.textbox_tchr_desig = new System.Windows.Forms.TextBox();
+            this.textbox_tchr_name = new System.Windows.Forms.TextBox();
+            this.textbox_tchr_ID = new System.Windows.Forms.TextBox();
+            this.textbox_stu_sem = new System.Windows.Forms.TextBox();
+            this.textbox_stu_dept = new System.Windows.Forms.TextBox();
+            this.textbox_stu_name = new System.Windows.Forms.TextBox();
+            this.textbox_tchr_dept = new System.Windows.Forms.TextBox();
+            this.button_tchr_save = new System.Windows.Forms.Button();
+            this.button_tchr_clr = new System.Windows.Forms.Button();
+            this.button_stu_clr = new System.Windows.Forms.Button();
+            this.listBox_stu = new System.Windows.Forms.ListBox();
+            this.listBox_tchr = new System.Windows.Forms.ListBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button_tchr_show = new System.Windows.Forms.Button();
+            this.button_stu_show = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(617, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Teacher Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(617, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Teacher ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Department";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Semester";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(60, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Student Name";
+            // 
+            // button_stu_save
+            // 
+            this.button_stu_save.Location = new System.Drawing.Point(72, 365);
+            this.button_stu_save.Name = "button_stu_save";
+            this.button_stu_save.Size = new System.Drawing.Size(75, 42);
+            this.button_stu_save.TabIndex = 6;
+            this.button_stu_save.Text = "Save";
+            this.button_stu_save.UseVisualStyleBackColor = true;
+            this.button_stu_save.Click += new System.EventHandler(this.button_stu_save_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(617, 246);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Designation";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(617, 194);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 20);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Department";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(617, 303);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 20);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Salary";
+            // 
+            // textBox_stu_ID
+            // 
+            this.textBox_stu_ID.Location = new System.Drawing.Point(153, 94);
+            this.textBox_stu_ID.Name = "textBox_stu_ID";
+            this.textBox_stu_ID.Size = new System.Drawing.Size(149, 26);
+            this.textBox_stu_ID.TabIndex = 10;
+            // 
+            // textbox_tchr_salary
+            // 
+            this.textbox_tchr_salary.Location = new System.Drawing.Point(676, 297);
+            this.textbox_tchr_salary.Name = "textbox_tchr_salary";
+            this.textbox_tchr_salary.Size = new System.Drawing.Size(149, 26);
+            this.textbox_tchr_salary.TabIndex = 11;
+            // 
+            // textbox_tchr_desig
+            // 
+            this.textbox_tchr_desig.Location = new System.Drawing.Point(717, 240);
+            this.textbox_tchr_desig.Name = "textbox_tchr_desig";
+            this.textbox_tchr_desig.Size = new System.Drawing.Size(149, 26);
+            this.textbox_tchr_desig.TabIndex = 12;
+            // 
+            // textbox_tchr_name
+            // 
+            this.textbox_tchr_name.Location = new System.Drawing.Point(736, 135);
+            this.textbox_tchr_name.Name = "textbox_tchr_name";
+            this.textbox_tchr_name.Size = new System.Drawing.Size(149, 26);
+            this.textbox_tchr_name.TabIndex = 13;
+            // 
+            // textbox_tchr_ID
+            // 
+            this.textbox_tchr_ID.Location = new System.Drawing.Point(711, 93);
+            this.textbox_tchr_ID.Name = "textbox_tchr_ID";
+            this.textbox_tchr_ID.Size = new System.Drawing.Size(149, 26);
+            this.textbox_tchr_ID.TabIndex = 14;
+            // 
+            // textbox_stu_sem
+            // 
+            this.textbox_stu_sem.Location = new System.Drawing.Point(144, 243);
+            this.textbox_stu_sem.Name = "textbox_stu_sem";
+            this.textbox_stu_sem.Size = new System.Drawing.Size(149, 26);
+            this.textbox_stu_sem.TabIndex = 15;
+            // 
+            // textbox_stu_dept
+            // 
+            this.textbox_stu_dept.Location = new System.Drawing.Point(160, 191);
+            this.textbox_stu_dept.Name = "textbox_stu_dept";
+            this.textbox_stu_dept.Size = new System.Drawing.Size(149, 26);
+            this.textbox_stu_dept.TabIndex = 16;
+            // 
+            // textbox_stu_name
+            // 
+            this.textbox_stu_name.Location = new System.Drawing.Point(178, 138);
+            this.textbox_stu_name.Name = "textbox_stu_name";
+            this.textbox_stu_name.Size = new System.Drawing.Size(149, 26);
+            this.textbox_stu_name.TabIndex = 17;
+            // 
+            // textbox_tchr_dept
+            // 
+            this.textbox_tchr_dept.Location = new System.Drawing.Point(717, 188);
+            this.textbox_tchr_dept.Name = "textbox_tchr_dept";
+            this.textbox_tchr_dept.Size = new System.Drawing.Size(149, 26);
+            this.textbox_tchr_dept.TabIndex = 18;
+            // 
+            // button_tchr_save
+            // 
+            this.button_tchr_save.Location = new System.Drawing.Point(676, 365);
+            this.button_tchr_save.Name = "button_tchr_save";
+            this.button_tchr_save.Size = new System.Drawing.Size(75, 42);
+            this.button_tchr_save.TabIndex = 19;
+            this.button_tchr_save.Text = "Save";
+            this.button_tchr_save.UseVisualStyleBackColor = true;
+            this.button_tchr_save.Click += new System.EventHandler(this.button_tchr_save_Click);
+            // 
+            // button_tchr_clr
+            // 
+            this.button_tchr_clr.Location = new System.Drawing.Point(785, 365);
+            this.button_tchr_clr.Name = "button_tchr_clr";
+            this.button_tchr_clr.Size = new System.Drawing.Size(75, 42);
+            this.button_tchr_clr.TabIndex = 20;
+            this.button_tchr_clr.Text = "Clear";
+            this.button_tchr_clr.UseVisualStyleBackColor = true;
+            this.button_tchr_clr.Click += new System.EventHandler(this.button_tchr_clr_Click);
+            // 
+            // button_stu_clr
+            // 
+            this.button_stu_clr.Location = new System.Drawing.Point(196, 365);
+            this.button_stu_clr.Name = "button_stu_clr";
+            this.button_stu_clr.Size = new System.Drawing.Size(75, 42);
+            this.button_stu_clr.TabIndex = 21;
+            this.button_stu_clr.Text = "Clear";
+            this.button_stu_clr.UseVisualStyleBackColor = true;
+            this.button_stu_clr.Click += new System.EventHandler(this.button_stu_clr_Click);
+            // 
+            // listBox_stu
+            // 
+            this.listBox_stu.FormattingEnabled = true;
+            this.listBox_stu.ItemHeight = 20;
+            this.listBox_stu.Location = new System.Drawing.Point(64, 468);
+            this.listBox_stu.Name = "listBox_stu";
+            this.listBox_stu.Size = new System.Drawing.Size(454, 264);
+            this.listBox_stu.TabIndex = 22;
+            this.listBox_stu.SelectedIndexChanged += new System.EventHandler(this.listBox_stu_SelectedIndexChanged);
+            // 
+            // listBox_tchr
+            // 
+            this.listBox_tchr.FormattingEnabled = true;
+            this.listBox_tchr.ItemHeight = 20;
+            this.listBox_tchr.Location = new System.Drawing.Point(621, 468);
+            this.listBox_tchr.Name = "listBox_tchr";
+            this.listBox_tchr.Size = new System.Drawing.Size(454, 264);
+            this.listBox_tchr.TabIndex = 23;
+            this.listBox_tchr.SelectedIndexChanged += new System.EventHandler(this.listBox_tchr_SelectedIndexChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(781, 32);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(94, 22);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "Teachers";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(192, 30);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(89, 22);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Students";
+            // 
+            // button_tchr_show
+            // 
+            this.button_tchr_show.Location = new System.Drawing.Point(897, 365);
+            this.button_tchr_show.Name = "button_tchr_show";
+            this.button_tchr_show.Size = new System.Drawing.Size(75, 42);
+            this.button_tchr_show.TabIndex = 26;
+            this.button_tchr_show.Text = "Show";
+            this.button_tchr_show.UseVisualStyleBackColor = true;
+            this.button_tchr_show.Click += new System.EventHandler(this.button_tchr_show_Click);
+            // 
+            // button_stu_show
+            // 
+            this.button_stu_show.Location = new System.Drawing.Point(307, 365);
+            this.button_stu_show.Name = "button_stu_show";
+            this.button_stu_show.Size = new System.Drawing.Size(75, 42);
+            this.button_stu_show.TabIndex = 27;
+            this.button_stu_show.Text = "Show";
+            this.button_stu_show.UseVisualStyleBackColor = true;
+            this.button_stu_show.Click += new System.EventHandler(this.button_stu_show_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 783);
+            this.Controls.Add(this.button_stu_show);
+            this.Controls.Add(this.button_tchr_show);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.listBox_tchr);
+            this.Controls.Add(this.listBox_stu);
+            this.Controls.Add(this.button_stu_clr);
+            this.Controls.Add(this.button_tchr_clr);
+            this.Controls.Add(this.button_tchr_save);
+            this.Controls.Add(this.textbox_tchr_dept);
+            this.Controls.Add(this.textbox_stu_name);
+            this.Controls.Add(this.textbox_stu_dept);
+            this.Controls.Add(this.textbox_stu_sem);
+            this.Controls.Add(this.textbox_tchr_ID);
+            this.Controls.Add(this.textbox_tchr_name);
+            this.Controls.Add(this.textbox_tchr_desig);
+            this.Controls.Add(this.textbox_tchr_salary);
+            this.Controls.Add(this.textBox_stu_ID);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button_stu_save);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button_stu_save;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox_stu_ID;
+        private System.Windows.Forms.TextBox textbox_tchr_salary;
+        private System.Windows.Forms.TextBox textbox_tchr_desig;
+        private System.Windows.Forms.TextBox textbox_tchr_name;
+        private System.Windows.Forms.TextBox textbox_tchr_ID;
+        private System.Windows.Forms.TextBox textbox_stu_sem;
+        private System.Windows.Forms.TextBox textbox_stu_dept;
+        private System.Windows.Forms.TextBox textbox_stu_name;
+        private System.Windows.Forms.TextBox textbox_tchr_dept;
+        private System.Windows.Forms.Button button_tchr_save;
+        private System.Windows.Forms.Button button_tchr_clr;
+        private System.Windows.Forms.Button button_stu_clr;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        public System.Windows.Forms.ListBox listBox_stu;
+        public System.Windows.Forms.ListBox listBox_tchr;
+        private System.Windows.Forms.Button button_tchr_show;
+        private System.Windows.Forms.Button button_stu_show;
+    }
+}
+
---
+++ Student-Teacher System/Student-Teacher System/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student_Teacher_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        List<Student> students = new List<Student>();
+        List<Teacher> teachers = new List<Teacher>();
+
+        private void button_stu_clr_Click(object sender, System.EventArgs e)
+        {
+            textbox_stu_dept.Clear();
+            textBox_stu_ID.Clear();
+            textbox_stu_name.Clear();
+            textbox_stu_sem.Clear();
+        }
+
+        private void button_tchr_clr_Click(object sender, System.EventArgs e)
+        {
+            textbox_tchr_dept.Clear();
+            textbox_tchr_desig.Clear();
+            textbox_tchr_ID.Clear();
+            textbox_tchr_name.Clear();
+            textbox_tchr_salary.Clear();
+        }
+
+        private void button_stu_save_Click(object sender, System.EventArgs e)
+        {
+            Student dummy = new Student();
+            dummy.setID(textBox_stu_ID.Text);
+            dummy.setname(textbox_stu_name.Text);
+            dummy.setdept(textbox_stu_dept.Text);
+            dummy.setsem(textbox_stu_sem.Text);
+            students.Add(dummy);
+            
+            MessageBox.Show("Added");
+
+        }
+
+        private void button_tchr_save_Click(object sender, System.EventArgs e)
+        {
+            Teacher dummy = new Teacher();
+            dummy.setID(textbox_tchr_ID.Text);
+            dummy.setname(textbox_tchr_name.Text);
+            dummy.setdesig(textbox_tchr_desig.Text);
+            dummy.setdept(textbox_tchr_dept.Text);
+            dummy.setsalary(textbox_tchr_salary.Text);
+            teachers.Add(dummy);
+            
+            MessageBox.Show("Added");
+        }
+
+        private void listBox_tchr_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void listBox_stu_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void button_stu_show_Click(object sender, System.EventArgs e)
+        {
+            listBox_stu.Items.Clear();
+            for (int i=0; i<students.Count; i++)
+            {
+                listBox_stu.Items.Add(students[i].getinfo());
+            }
+        }
+
+        private void button_tchr_show_Click(object sender, System.EventArgs e)
+        {
+            listBox_tchr.Items.Clear();
+            for (int i=0; i<teachers.Count; i++)
+            {
+                listBox_tchr.Items.Add(teachers[i].getinfo());
+            }
+        }
+    }
+    
+    
+}
