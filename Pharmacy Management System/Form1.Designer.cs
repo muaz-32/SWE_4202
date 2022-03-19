552a85a16973a0bf43fb504975b8da56e53989ca
muaz-32 <96006730+muaz-32@users.noreply.github.com>
2022-03-19 15:01:19 +0600

    Add files via upload

---
+++ Pharmacy Management System/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace Pharmacy_Management_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button_medicine_add = new System.Windows.Forms.Button();
+            this.textBox_medicine_name = new System.Windows.Forms.TextBox();
+            this.textBox_accessories_name = new System.Windows.Forms.TextBox();
+            this.textBox_medicine_cost = new System.Windows.Forms.TextBox();
+            this.textBox_medicine_qty = new System.Windows.Forms.TextBox();
+            this.textBox_buy_name = new System.Windows.Forms.TextBox();
+            this.textBox_buy_type = new System.Windows.Forms.TextBox();
+            this.textBox_accessories_cost = new System.Windows.Forms.TextBox();
+            this.textBox_buy_qty = new System.Windows.Forms.TextBox();
+            this.button_accessories_add = new System.Windows.Forms.Button();
+            this.button_buy = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox_balance = new System.Windows.Forms.TextBox();
+            this.button_balance = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_accessories_qty = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Medicine";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(459, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(459, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Accessories";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(110, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Cost";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(110, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Quantity";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(110, 180);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Name";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(817, 121);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Buy";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(459, 303);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 25);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Cost";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(817, 303);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(92, 25);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Quantity";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(817, 241);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(68, 25);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Name";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(817, 180);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(234, 25);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Medicine / Accessories";
+            // 
+            // button_medicine_add
+            // 
+            this.button_medicine_add.Location = new System.Drawing.Point(172, 377);
+            this.button_medicine_add.Name = "button_medicine_add";
+            this.button_medicine_add.Size = new System.Drawing.Size(97, 35);
+            this.button_medicine_add.TabIndex = 13;
+            this.button_medicine_add.Text = "Add";
+            this.button_medicine_add.UseVisualStyleBackColor = true;
+            this.button_medicine_add.Click += new System.EventHandler(this.button_medicine_add_Click);
+            // 
+            // textBox_medicine_name
+            // 
+            this.textBox_medicine_name.Location = new System.Drawing.Point(184, 177);
+            this.textBox_medicine_name.Name = "textBox_medicine_name";
+            this.textBox_medicine_name.Size = new System.Drawing.Size(100, 31);
+            this.textBox_medicine_name.TabIndex = 14;
+            // 
+            // textBox_accessories_name
+            // 
+            this.textBox_accessories_name.Location = new System.Drawing.Point(533, 177);
+            this.textBox_accessories_name.Name = "textBox_accessories_name";
+            this.textBox_accessories_name.Size = new System.Drawing.Size(100, 31);
+            this.textBox_accessories_name.TabIndex = 15;
+            // 
+            // textBox_medicine_cost
+            // 
+            this.textBox_medicine_cost.Location = new System.Drawing.Point(172, 300);
+            this.textBox_medicine_cost.Name = "textBox_medicine_cost";
+            this.textBox_medicine_cost.Size = new System.Drawing.Size(100, 31);
+            this.textBox_medicine_cost.TabIndex = 16;
+            // 
+            // textBox_medicine_qty
+            // 
+            this.textBox_medicine_qty.Location = new System.Drawing.Point(208, 241);
+            this.textBox_medicine_qty.Name = "textBox_medicine_qty";
+            this.textBox_medicine_qty.Size = new System.Drawing.Size(100, 31);
+            this.textBox_medicine_qty.TabIndex = 17;
+            // 
+            // textBox_buy_name
+            // 
+            this.textBox_buy_name.Location = new System.Drawing.Point(891, 238);
+            this.textBox_buy_name.Name = "textBox_buy_name";
+            this.textBox_buy_name.Size = new System.Drawing.Size(100, 31);
+            this.textBox_buy_name.TabIndex = 19;
+            // 
+            // textBox_buy_type
+            // 
+            this.textBox_buy_type.Location = new System.Drawing.Point(1057, 177);
+            this.textBox_buy_type.Name = "textBox_buy_type";
+            this.textBox_buy_type.Size = new System.Drawing.Size(100, 31);
+            this.textBox_buy_type.TabIndex = 20;
+            // 
+            // textBox_accessories_cost
+            // 
+            this.textBox_accessories_cost.Location = new System.Drawing.Point(521, 300);
+            this.textBox_accessories_cost.Name = "textBox_accessories_cost";
+            this.textBox_accessories_cost.Size = new System.Drawing.Size(100, 31);
+            this.textBox_accessories_cost.TabIndex = 21;
+            // 
+            // textBox_buy_qty
+            // 
+            this.textBox_buy_qty.Location = new System.Drawing.Point(915, 300);
+            this.textBox_buy_qty.Name = "textBox_buy_qty";
+            this.textBox_buy_qty.Size = new System.Drawing.Size(100, 31);
+            this.textBox_buy_qty.TabIndex = 23;
+            // 
+            // button_accessories_add
+            // 
+            this.button_accessories_add.Location = new System.Drawing.Point(464, 377);
+            this.button_accessories_add.Name = "button_accessories_add";
+            this.button_accessories_add.Size = new System.Drawing.Size(97, 35);
+            this.button_accessories_add.TabIndex = 24;
+            this.button_accessories_add.Text = "Add";
+            this.button_accessories_add.UseVisualStyleBackColor = true;
+            this.button_accessories_add.Click += new System.EventHandler(this.button_accessories_add_Click);
+            // 
+            // button_buy
+            // 
+            this.button_buy.Location = new System.Drawing.Point(822, 377);
+            this.button_buy.Name = "button_buy";
+            this.button_buy.Size = new System.Drawing.Size(97, 35);
+            this.button_buy.TabIndex = 25;
+            this.button_buy.Text = "Buy";
+            this.button_buy.UseVisualStyleBackColor = true;
+            this.button_buy.Click += new System.EventHandler(this.button_buy_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(817, 441);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(90, 25);
+            this.label14.TabIndex = 26;
+            this.label14.Text = "Balance";
+            // 
+            // textBox_balance
+            // 
+            this.textBox_balance.Location = new System.Drawing.Point(913, 438);
+            this.textBox_balance.Name = "textBox_balance";
+            this.textBox_balance.ReadOnly = true;
+            this.textBox_balance.Size = new System.Drawing.Size(100, 31);
+            this.textBox_balance.TabIndex = 27;
+            // 
+            // button_balance
+            // 
+            this.button_balance.Location = new System.Drawing.Point(1042, 436);
+            this.button_balance.Name = "button_balance";
+            this.button_balance.Size = new System.Drawing.Size(176, 35);
+            this.button_balance.TabIndex = 28;
+            this.button_balance.Text = "Balance";
+            this.button_balance.UseVisualStyleBackColor = true;
+            this.button_balance.Click += new System.EventHandler(this.button_balance_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(459, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 25);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Quantity";
+            // 
+            // textBox_accessories_qty
+            // 
+            this.textBox_accessories_qty.Location = new System.Drawing.Point(557, 241);
+            this.textBox_accessories_qty.Name = "textBox_accessories_qty";
+            this.textBox_accessories_qty.Size = new System.Drawing.Size(100, 31);
+            this.textBox_accessories_qty.TabIndex = 31;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1337, 761);
+            this.Controls.Add(this.textBox_accessories_qty);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button_balance);
+            this.Controls.Add(this.textBox_balance);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.button_buy);
+            this.Controls.Add(this.button_accessories_add);
+            this.Controls.Add(this.textBox_buy_qty);
+            this.Controls.Add(this.textBox_accessories_cost);
+            this.Controls.Add(this.textBox_buy_type);
+            this.Controls.Add(this.textBox_buy_name);
+            this.Controls.Add(this.textBox_medicine_qty);
+            this.Controls.Add(this.textBox_medicine_cost);
+            this.Controls.Add(this.textBox_accessories_name);
+            this.Controls.Add(this.textBox_medicine_name);
+            this.Controls.Add(this.button_medicine_add);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button_medicine_add;
+        private System.Windows.Forms.TextBox textBox_medicine_name;
+        private System.Windows.Forms.TextBox textBox_accessories_name;
+        private System.Windows.Forms.TextBox textBox_medicine_cost;
+        private System.Windows.Forms.TextBox textBox_medicine_qty;
+        private System.Windows.Forms.TextBox textBox_buy_name;
+        private System.Windows.Forms.TextBox textBox_buy_type;
+        private System.Windows.Forms.TextBox textBox_accessories_cost;
+        private System.Windows.Forms.TextBox textBox_buy_qty;
+        private System.Windows.Forms.Button button_accessories_add;
+        private System.Windows.Forms.Button button_buy;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox_balance;
+        private System.Windows.Forms.Button button_balance;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_accessories_qty;
+    }
+}
+
---
+++ Pharmacy Management System/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pharmacy_Management_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Medicine> medicines = new List<Medicine>();
+        List<Accessories> accessories = new List<Accessories>();
+        public int money = 0;
+
+        private void button_medicine_add_Click(object sender, EventArgs e)
+        {
+            Medicine medicine = new Medicine();
+            medicine.name = textBox_medicine_name.Text;
+            medicine.qty = Convert.ToInt32(textBox_medicine_qty.Text);
+            medicine.cost = Convert.ToInt32(textBox_medicine_cost.Text);
+            medicines.Add(medicine);
+        }
+
+        private void button_accessories_add_Click(object sender, EventArgs e)
+        {
+            Accessories accessory = new Accessories();
+            accessory.name = textBox_accessories_name.Text;
+            accessory.qty = Convert.ToInt32(textBox_accessories_qty.Text);
+            accessory.cost = Convert.ToInt32(textBox_accessories_cost.Text);
+            accessories.Add(accessory);
+        }
+
+        private void button_buy_Click(object sender, EventArgs e)
+        {
+            if(textBox_buy_type.Text == "Medicine")
+            {
+                for(int i = 0; i < medicines.Count; i++)
+                {
+                    if(medicines[i].name == textBox_buy_name.Text)
+                    {
+                        money += medicines[i].cost * Convert.ToInt32(textBox_buy_qty.Text);
+                        medicines[i].qty -= Convert.ToInt32(textBox_buy_qty.Text);
+                    }
+                }
+            }
+            else if(textBox_buy_type.Text == "Accessory")
+            {
+                for (int i = 0; i < accessories.Count; i++)
+                {
+                    if (accessories[i].name == textBox_buy_name.Text)
+                    {
+                        money += accessories[i].cost * Convert.ToInt32(textBox_buy_qty.Text);
+                        accessories[i].qty -= Convert.ToInt32(textBox_buy_qty.Text);
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private void button_balance_Click(object sender, EventArgs e)
+        {
+            textBox_balance.Text = Convert.ToString(money);
+        }
+    }
+    internal class Medicine
+    {
+        public string name;
+        public int qty;
+        public int cost;
+    }
+    internal class Accessories
+    {
+        public string name;
+        public int qty;
+        public int cost;
+    }
+}
